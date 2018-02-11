7dbcfc684253b6d56d3a9658f224bab8d4d1833f
hansy <hansy0419@thundersoft.com>
2018-02-11 15:28:09 +0800

    modify redis script by Sanyang

--- benchmarks/redis/files/run_server_ubuntu.sh
+++ benchmarks/redis/files/run_server_ubuntu.sh
@@ -8,4 +8,4 @@ start_cpu_num=1
 inst_num=2
 end_cpu_num=$[$start_cpu_num+$inst_num-1]
 
-scripts/start_server_ubuntu.sh $start_cpu_num $end_cpu_num
+/tmp/redis/files/start_server_ubuntu.sh $start_cpu_num $end_cpu_num
--- benchmarks/redis/handlers/redis_parser.py
+++ benchmarks/redis/handlers/redis_parser.py
@@ -1,95 +1,87 @@
 import re
 import string
-import pdb
+import yaml
+import json
+from caliper.server.parser_process import parser_log
 
-def redis_parser(content , outfp ):
-#[test: Instance_2]
+
+def redis_parser(content, outfp):
+    # [test: Instance_2]
 
     for test_case in re.findall("\[test:\s+(.*?)\]", content):
-	test_case_latency = test_case + "_latency"
-	test_case_bandwidth = test_case + "_bandwidth"
-  
+        test_case_bandwidth = test_case
+
     dic = {}
-    dic[test_case_latency] = {}
     dic[test_case_bandwidth] = {}
-    dic[test_case_latency]['short-lat'] = 0
-    dic[test_case_latency]['basic-lat'] = 0
-    dic[test_case_latency]['pipeline-lat'] = 0
     dic[test_case_bandwidth]['short-qps'] = 0
     dic[test_case_bandwidth]['basic-qps'] = 0
     dic[test_case_bandwidth]['pipeline-qps'] = 0
 
-    short_count = 0
-    basic_count = 0
-    pipeline_count = 0
-    for contents in re.findall("====== SHORT ======.*?(\d+.\d+)\s+requests per second", content, re.DOTALL):
-         short_final = string.atof(contents.strip())
-         outfp.write("short-qps = %s \n " %  short_final)
-         dic[test_case_bandwidth]['short-qps'] += short_final
+    for contents in re.findall("Short case\nqps:.*?(\d+.\d+), lat:", content, re.DOTALL):
+        short_final = string.atof(contents.strip())
+        outfp.write("short-qps = %s \n " % short_final)
+        dic[test_case_bandwidth]['short-qps'] += short_final
 
-    for contents in re.findall("====== BASIC ======.*?(\d+.\d+)\s+requests per second", content, re.DOTALL):
+    for contents in re.findall("Basic case\nqps:.*?(\d+.\d+), lat:", content, re.DOTALL):
         basic_final = string.atof(contents.strip())
         outfp.write("basic-qps = %s \n " % basic_final)
         dic[test_case_bandwidth]['basic-qps'] += basic_final
 
-    for contents in re.findall("====== PIPELINE ======.*?(\d+.\d+)\s+requests per second", content, re.DOTALL):
+    for contents in re.findall("Pipeline case\nqps:.*?(\d+.\d+), lat:", content, re.DOTALL):
         pipeline_final = string.atof(contents.strip())
         outfp.write("pipeline-qps = %s \n\n " % pipeline_final)
         dic[test_case_bandwidth]['pipeline-qps'] += pipeline_final
 
-    for contents in re.findall("====== SHORT ======.*?[9][0-9]\..*?%\s<=\s(\d+)\s+milliseconds", content, re.DOTALL):
-        short_final = string.atof(contents.strip())
-        outfp.write("short-lat = %s \n " %  short_final)
-        dic[test_case_latency]['short-lat'] += short_final
-	short_count += 1
-
-    for contents in re.findall("====== BASIC ======.*?[9][0-9]\..*?%\s<=\s(\d+)\s+milliseconds", content, re.DOTALL):
-        basic_final = string.atof(contents.strip())
-        outfp.write("basic-lat = %s \n " % basic_final)
-        dic[test_case_latency]['basic-lat'] += basic_final
-	basic_count += 1
-
-    for contents in re.findall("====== PIPELINE ======.*?[9][0-9]\..*?%\s<=\s(\d+)\s+milliseconds", content, re.DOTALL):
-        pipeline_final = string.atof(contents.strip())
-        outfp.write("pipeline-lat = %s \n\n " % pipeline_final)
-        dic[test_case_latency]['pipeline-lat'] += pipeline_final
-	pipeline_count += 1
-
-    if short_count != 0:
-    	dic[test_case_latency]['short-lat'] = dic[test_case_latency]['short-lat'] / short_count
-
-    if basic_count != 0:
-    	dic[test_case_latency]['basic-lat'] = dic[test_case_latency]['basic-lat'] / basic_count
-
-    if pipeline_count != 0:
-    	dic[test_case_latency]['pipeline-lat'] = dic[test_case_latency]['pipeline-lat'] / pipeline_count
-
     if dic[test_case_bandwidth]['short-qps'] == 0:
-	dic[test_case_bandwidth]['short-qps'] = -1
+        dic[test_case_bandwidth]['short-qps'] = -1
 
     if dic[test_case_bandwidth]['basic-qps'] == 0:
-	dic[test_case_bandwidth]['basic-qps'] = -1
+        dic[test_case_bandwidth]['basic-qps'] = -1
 
     if dic[test_case_bandwidth]['pipeline-qps'] == 0:
-	dic[test_case_bandwidth]['pipeline-qps'] = -1
-
-    if dic[test_case_latency]['short-lat'] == 0:
-	dic[test_case_latency]['short-lat'] = -1
-
-    if dic[test_case_latency]['basic-lat'] == 0:
-	dic[test_case_latency]['basic-lat'] = -1
-
-    if dic[test_case_latency]['pipeline-lat'] == 0:
-	dic[test_case_latency]['pipeline-lat'] = -1
+        dic[test_case_bandwidth]['pipeline-qps'] = -1
 
     return dic
+
+
+def redis(filePath, outfp):
+    cases = parser_log.parseData(filePath)
+    result = []
+    for case in cases:
+        caseDict = {}
+        caseDict[parser_log.BOTTOM] = parser_log.getBottom(case)
+        titleGroup = re.search("\[test:([\s\S]+?)\]", case)
+        if titleGroup != None:
+            caseDict[parser_log.TOP] = titleGroup.group(0)
+        tables = []
+        tableContent = {}
+        centerTopGroup = re.search("log:([\s\S]+?)\n", case)
+        tableContent[parser_log.CENTER_TOP] = centerTopGroup.groups()[0]
+        tableGroup = re.search("Short case([\s\S]+)status", case)
+        if tableGroup is not None:
+            tableGroupContent_temp = tableGroup.groups()[0].strip().replace('[', '')
+            tableGroupContent = re.sub('=', ':', tableGroupContent_temp)
+            tableGroupContent = re.sub('lat:', 'lat=', tableGroupContent)
+            table = parser_log.parseTable(tableGroupContent, ":{1,}")
+            tableContent[parser_log.I_TABLE] = table
+        tables.append(tableContent)
+        caseDict[parser_log.TABLES] = tables
+        result.append(caseDict)
+    outfp.write(json.dumps(result))
+    return result
+
 
 if __name__ == "__main__":
     infp = open("redis_output.log", "r")
     content = infp.read()
-    content = re.findall(r'<<<BEGIN TEST>>>(.*?)<<<END>>>',content,re.DOTALL)
+    content = re.findall(r'<<<BEGIN TEST>>>(.*?)<<<END>>>', content, re.DOTALL)
     outfp = open("2.txt", "a+")
     for data in content:
         a = redis_parser(data, outfp)
     outfp.close()
     infp.close()
+    infile = "redis_output.log"
+    outfile = "redis.json"
+    outfp = open(outfile, "a+")
+    redis(infile, outfp)
+    outfp.close()
